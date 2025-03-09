66fc125870aeaf3cd0092846c35a2ea7f06cb57d
sou.hoc <souhoc@icloud.com>
2025-03-09 14:34:06 +0100

    refactor: better naming const

--- constants.go
+++ constants.go
@@ -1,11 +1,11 @@
 package goft
 
 const (
-	BaseUrl = "https://api.intra.42.fr"
+	BaseURL = "https://api.intra.42.fr"
 
 	Version          = "v2"
-	BaseUrlVersioned = BaseUrl + "/" + Version
+	BaseUrlVersioned = BaseURL + "/" + Version
 
-	AuthUrl  = BaseUrl + "/oauth/authorize"
-	TokenUrl = BaseUrl + "/oauth/token"
+	AuthURL  = BaseURL + "/oauth/authorize"
+	TokenURL = BaseURL + "/oauth/token"
 )
